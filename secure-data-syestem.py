93a3133eae5665aab521b359bb95ee044e0254ef
MuhammadRayyan45 <rayyanrk445@gmail.com>
2025-04-26 18:07:38 -0700

    secure-data-syestem.py

--- secure-data-syestem.py
+++ secure-data-syestem.py
@@ -89,7 +89,7 @@ elif choice == "Register":
                 st.success("✅ User register sucessfully!")
         else:
             st.error("Both fields are required.")
-    elif choice == "Login":
+elif choice == "Login":
         st.subheader("🔑 User Login")
 
         if time.time() < st.session_state.lockout_time:
